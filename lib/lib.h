5cc5f3bfd5358fcd81c9fd5cc1f1f69bd9d96e66
Pedro Falcato <pedro.falcato@gmail.com>
2023-07-19 20:00:30 +0100

    lib/lib.h: Make WARN_ONLY an unsigned int
    1 << 31 is UB as "1" is a signed int (and it overflows).
    
    Caught using UBSAN.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- lib/lib.h
+++ lib/lib.h
@@ -93,7 +93,7 @@ struct dirtree *dirtree_read(char *path, int (*callback)(struct dirtree *node));
 // Tell xopen and friends to print warnings but return -1 as necessary
 // The largest O_BLAH flag so far is arch/alpha's O_PATH at 0x800000 so
 // plenty of headroom.
-#define WARN_ONLY        (1<<31) // don't exit, just warn
+#define WARN_ONLY        (1U<<31) // don't exit, just warn
 #define LOOPFILES_ANYWAY (1<<30) // call function with fd -1
 
 // xabspath flags
